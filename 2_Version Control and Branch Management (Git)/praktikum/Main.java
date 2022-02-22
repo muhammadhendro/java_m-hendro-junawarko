b44f80cad22c5d6df9e5638b7ebdb679fe249c39
muhammadhendro <muhammadhendro88@gmail.com>
2022-02-22 12:21:55 +0700

    fixing stash feature B.sub B.1

--- 2_Version Control and Branch Management (Git)/praktikum/Main.java
+++ 2_Version Control and Branch Management (Git)/praktikum/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Feature B");
         System.out.println("Feature B.sub");
+        System.out.println("Feature B.1");
     }
 }
